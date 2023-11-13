8846a79679474416dd7a19665b1b915aa9b77ca0
paulaechemendia <140556972+paulaechemendia@users.noreply.github.com>
2023-11-13 19:49:49 +0000

    SE HA HECHO LO QUE SE HA PODIDO

--- src/app.py
+++ src/app.py
@@ -1,10 +1,8 @@
-
 import pandas as pd
 
 
 
 df = pd.read_csv('/workspaces/machine-learning-python-template/data/raw/data.csv')
-
-print(df.info())
-
-print(df.shape)
+drops = ['id', 'host_id', 'name', 'host_name', 'neighbourhood', 'last_review']
+df = df.drop(drops, axis=1)
+print(df.describe())
